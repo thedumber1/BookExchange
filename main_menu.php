e95288ff131d3b13989c24d4a5cf2c4e14bccd19
Anthony <thebatmang12@gmail.com>
2021-01-16 21:02:41 -0800

    updated with bootstrap

--- main_menu.php
+++ main_menu.php
@@ -1,6 +1,6 @@
-<link href="main.css" type="text/css" rel="stylesheet"/>
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,700" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link href="main.css" type="text/css" rel="stylesheet"/>
 
 <?php
     session_start();
@@ -25,22 +25,23 @@
         <li class="nav-item">
           <a class="nav-link" href="mybooks.php">My Books</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="profile.php">Profile</a>
-        </li>
-        <form class="d-flex" action="#">
-          <input class="form-control me-2 ms-5" type="search" placeholder="Search" aria-label="Search">
+        
+        <form class="d-flex forms" action="#">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
     </ul>
     <ul class="navbar-nav mb-2 mb-lg-0">
         <li class="nav-item">
             <a class="nav-link" href="cart_view.php">
-                <img src="shoppingcart.png" width="25" height="20">
+                <img src="Pictures/shoppingcart.png" width="25" height="20">
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Logout</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="profile.php">Profile</a>
         </li>
     </ul>
     </div>
--- selling.php
+++ selling.php
@@ -1,57 +1,67 @@
 <!doctype html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<title>Selling Page</title>
+	<meta charset="utf-8">
+	<title>Selling Page</title>
 </head>
 
 <body>
-    <?php
-        include("main_menu.php");
-    ?>
-	<h1 align="center">
-        Please submit your book information
-	</h1>
-	<form action="sellingInsert.php" enctype="multipart/form-data" method="post">		
-		<table align="center" border="2">
-			<tr>
-				<td>
-					Email
-				</td>
-				<td>
-					<input type="text" name="email" value="<?php print $_SESSION["email"];?>">
-				</td>
-			</tr>
-			<tr>
-				<td>
-					Book Name
-				</td>
-				<td>
-					<input type="text" name="name">
-				</td>
-            </tr>
-            <tr>
-				<td>
-					Description
-				</td>
-				<td>
-					<input type="text" name="description">
-				</td>
-            </tr>
-            <tr>
-				<td>
-					Book Picture
-				</td>
-				<td>
-					<input type="file" name="pic">
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="submit" value="submit">
-				</td>
-			</tr>
-		</table>
-	</form>
+	<?php
+	include("main_menu.php");
+	?>
+	<h1 class="text-center my-3">Please submit your book information</h1>
+
+	<div class="container">
+		<form action="sellingInsert.php" enctype="multipart/form-data" method="post" id="test">
+			<div class="justify-content-end">
+			<div class="row justify-content-end">
+				<div class="col-6">
+					<div class="mb-3">
+						<label for="exampleInputEmail1" class="form-label">Email address</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value="<?php print $_SESSION["email"]; ?>">
+						<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-end">
+				<div class="col-6">
+					<div class="mb-3">
+						<label for="bookName" class="form-label">Book Name</label>
+						<input type="text" class="form-control" name="name" id="bookName">
+						<div class="form-text">Name of the book</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-end p-0">
+				<div class="col-6">
+					<div class="mb-3">
+						<label for="description" class="form-label">Description</label>
+						<input type="text" class="form-control" name="description">
+						<div class="form-text">Brief description of the book.</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-end p-0">
+				<div class="col-6">
+					<div class="mb-3">
+						<label for="picture" class="form-label">Book Picture</label>
+						<input type="file" class="form-control" name="pic">
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-end p-0">
+				<div class="col-6">
+					<button type="submit" class="btn btn-primary btn-md">Submit</button>
+				</div>
+			</div>
+			</div>
+			
+		</form>
+
+	</div>
 </body>
+
 </html>
