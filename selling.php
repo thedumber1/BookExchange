3ee4c0bee1f79f3b6e876dcbc8547fe29a4cb361
Anthony <thebatmang12@gmail.com>
2021-01-24 09:18:57 -0800

    updated

--- selling.php
+++ selling.php
@@ -8,50 +8,54 @@
 <body>
     <?php
         include("main_menu.php");
-    ?>
-	<h1 align="center">
-        Please submit your book information
-	</h1>
-	<form action="sellingInsert.php" enctype="multipart/form-data" method="post">		
-		<table align="center" border="2">
-			<tr>
-				<td>
-					Email
-				</td>
-				<td>
-					<input type="text" name="email" value="<?php print $_SESSION["email"];?>">
-				</td>
-			</tr>
-			<tr>
-				<td>
-					Book Name
-				</td>
-				<td>
-					<input type="text" name="name">
-				</td>
-            </tr>
-            <tr>
-				<td>
-					Description
-				</td>
-				<td>
-					<input type="text" name="description">
-				</td>
-            </tr>
-            <tr>
-				<td>
-					Book Picture
-				</td>
-				<td>
-					<input type="file" name="pic">
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="submit" value="submit">
-				</td>
-			</tr>
-		</table>
-	</form>
+	?>
+	<div class="container">
+		<h1>Please submit your book information</h1>
+
+	</div>
+
+	<div class="container">
+		<form class="sellForm" action="sellingInsert.php" enctype="multipart/form-data" method="post">		
+			<table class="sellTable">
+				<tr>
+					<td>
+						<label for="email" class="">Email</label>
+					</td>
+					<td>
+						<input type="text" name="email" value="<?php print $_SESSION["email"];?>">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="name" class="">Book Name</label>
+					</td>
+					<td>
+						<input type="text" name="name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="description" class="">Description</label>
+					</td>
+					<td>
+						<input type="text" name="description">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="pic" class="">Book Picture</label>
+					</td>
+					<td>
+						<input type="file" name="pic">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<button class="sellButton" type="submit">Submit</button>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
 </body>
 </html>
